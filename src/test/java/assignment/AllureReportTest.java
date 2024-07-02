c9bb81aad80bcc99a516b55dbfc206451e3131c9
shankar2409 <shankarraman1998@gmail.com>
2024-07-02 19:54:26 +0530

    smnslkgrjesgkhesg

--- src/test/java/assignment/AllureReportTest.java
+++ src/test/java/assignment/AllureReportTest.java
@@ -1,17 +1,21 @@
 package assignment;
 
+import org.junit.Assert;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import com.hms.genericUtils.BaseClass;
 
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 
-public class AllureReportTest {
+public class AllureReportTest extends BaseClass {
 	@Epic("epic")
 	@Story("story")
 	@Test
 	public void demo1() {
-		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://allurereport.org/docs/integrations-jenkins/");
+		Assert.fail();
 		System.out.println("chrome launched");
 	}
 	@Epic("epic1")
@@ -19,6 +23,8 @@ public class AllureReportTest {
 	@Test
 	public void demo2() {
 		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://allurereport.org/docs/integrations-jenkins/");
+		Assert.fail();
 		System.out.println("chrome launched");
 	}
 }
